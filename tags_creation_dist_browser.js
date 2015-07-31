e9cf0b0a1d8618a14ea806cb822e409e04d76bb8
Muhammad <muhammad@matterandform.net>
2015-07-31 14:27:40 -0400

    tag doubles in creations page + documentation

--- tags_creation_dist_browser.js
+++ tags_creation_dist_browser.js
@@ -1,11 +1,28 @@
+/*	Muhammad Mousa
+	
+	A program that you paste in the browser when you navigate to
+	/creations on cashew. Recommended to set creation limit to the
+	max number of creations in API, Frontend and Router until a 
+	better fix is found.
+*/
+
 var creations = app.collections.creations = app.collections.creations || new CreationsCollection();
-var arr, sub_arr = [];
+// assign the creations to a variable
+
+var arr, sub_arr;
+// sub_arr is a tag double array
+// arr is an array of array doubles for all creations
+
 var tags;
+// array of tags for each creation
+
+arr = [];
+
 for (var i in creations.models) {
 	tags = creations.models[i]._values.tags;
-	//arr=[];
+	arr=[];
 	for (var j=0; j < tags.length; j++) {
-		var sub_arr = [];
+		sub_arr = [];
 		for (var k = j+1; k < tags.length; k++) {
 			sub_arr.push(tags[j]);
 			sub_arr.push(tags[k]);
@@ -14,17 +31,16 @@ for (var i in creations.models) {
 			sub_arr=[];
 		}
 	}
-	
 }
 console.log(arr);
 
+
+/*
 for (var i=0; i<arr.length; i++) {
 	for (var j=i+1; j<arr.length; j++) {
 		
 	}
 }
-
-// copy paste all this in browser when u go to creations
 /*
 
 var min = function (a,b,c) {
