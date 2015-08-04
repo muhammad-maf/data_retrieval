3d1ff8d6a738ab229ed596e18df4cef1f0123e1a
Muhammad <muhammad@matterandform.net>
2015-08-04 12:04:41 -0400

    add changes in triples program (many issues)

--- tags_creation_dist_triples.js
+++ tags_creation_dist_triples.js
@@ -4,15 +4,38 @@
 	Very experimental :p
 */
 
+var min = function (a,b,c) {
+	if (a < b && a < c) return a;
+	if (b < a && b < c) return b;
+	else return c;
+}
+
+var lev_dist = function (str1, str2) {
+    var m = str1.length,
+        n = str2.length,
+        d = [],
+        i, j;
+ 
+    if (!m) return n;
+    if (!n) return m;
+ 
+    for (i = 0; i <= m; i++) d[i] = [i];
+    for (j = 0; j <= n; j++) d[0][j] = j;
+ 
+    for (j = 1; j <= n; j++) {
+        for (i = 1; i <= m; i++) {
+            if (str1[i-1] == str2[j-1]) d[i][j] = d[i - 1][j - 1];
+            else d[i][j] = Math.min(d[i-1][j], d[i][j-1], d[i-1][j-1]) + 1;
+        }
+    }
+    return d[m][n];
+}
+
 var creations = app.collections.creations = app.collections.creations || new CreationsCollection();
-// assign the creations to a variable
 
 var creations_arr, tags_arr;
-// sub_arr is a tag triples array
-// arr is an array of array triples for all creations
 
 var tags;
-// array of tags for each creation
 
 creations_arr = [];
 
@@ -25,45 +48,43 @@ for (var i in creations.models) {
 		tags_arr = [];
 		for (var k = j+1; k < tags.length; k++) {
 			for (var l = k+1; l < tags.length; l++) {
-				tags_arr.push(tags[j]);
-				tags_arr.push(tags[k]);
-				tags_arr.push(tags[l]);
-				tags_arr.sort();
-				creations_arr.push(tags_arr);
-				tags_arr=[];
+				if (tags[j]!==tags[k] && tags[j]!==tags[l] && tags[k]!==tags[l]) {
+					tags_arr.push(tags[j]);
+					tags_arr.push(tags[k]);
+					tags_arr.push(tags[l]);
+					tags_arr.sort();
+					creations_arr.push(tags_arr);
+					tags_arr=[];
+				}
 			}
 		}
 	}
 }
-// populate creations_arr with all the combinations of tag triples
-
- console.log(creations_arr);
 
 var tag_triples_count = creations_arr.length;
 // number of tag triples
 
-const popular_group_tag_limit = Math.floor(Math.log(tag_triples_count) / Math.log(10));
-// the number of occurrences of a group of tags necessary to make them "popular"
-// Note: The math makes this program scalable so that it adds 1 to popular_group_tag_limit
-//     every time the number of tag triples increases by an order of magnitude in base 10
+const pop_group_tag_lim = Math.floor(Math.log(tag_triples_count) / Math.log(10));
+
+const leven_lim = 3;
 
 var group_tag_count = 0;
-// number of occurrences of a group of tags in arr
 
 var popular_tag_groups = [];
 
 for (var i=0; i<creations_arr.length; i++) {
 	var cur_tag_group_str = creations_arr[i].join("");
-	// parse tags array (triples) as string for easy comparisons; this works as tags array is sorted
+	// parse tags array (double) as string for easy comparisons; this works as tags array is sorted
 	for (var j=i+1; j<creations_arr.length; j++) {
 		var comp_tag_group_str = creations_arr[j].join("");
-		if (cur_tag_group_str === comp_tag_group_str) {
-			console.log(cur_tag_group_str + " " + comp_tag_group_str);
+		if (lev_dist(comp_tag_group_str, cur_tag_group_str) <= leven_lim) {
 			group_tag_count++;
 		}
-		if (group_tag_count == popular_group_tag_limit) {
-			popular_tag_groups.push(creations_arr[i]);
+		if (group_tag_count === pop_group_tag_lim) {
 			group_tag_count=0;
+			if (popular_tag_groups.join("").indexOf(creations_arr[j]) === -1) {
+				popular_tag_groups.push(creations_arr[j]);
+			}
 		}
 	}
 	group_tag_count=0;
