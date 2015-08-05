2e40e98be8c29ab01df333dd65dc313ab5e0d98b
Muhammad <muhammad@matterandform.net>
2015-08-05 16:18:58 -0400

    combinations are in collection

--- tag-map-reduce/index.js
+++ tag-map-reduce/index.js
@@ -6,9 +6,29 @@ MongoClient.connect('mongodb://127.0.0.1:27017/matter-and-form-api', function(er
     var creationsCollection = db.collection('creations');
 
     function mapFunc() {
-    	this.tags.forEach(function(tag) {
-    		emit(tag, 1);
-    	});
+        var creations_arr = [];
+    	for (var tag in this) {
+            tags = this.tags;
+            if (tags.join("") === "1234567891011121314151617181920212223242526") {
+                continue;
+            }
+            // literally to work around Michael's broken creation
+            // need a fix to ignore tags with just numbers 
+            for (var j=0; j < tags.length; j++) {
+                tags_arr = [];
+                for (var k = j+1; k < tags.length; k++) {
+                    if (tags[j]!==tags[k]) {
+                        // No duplicates like ["space", "space"] - we don't want a tag to be identified as being similar to itself 
+                        emit(tag, [tags[k], tags[k]]);
+                        tags_arr.push(tags[j]);
+                        tags_arr.push(tags[k]);
+                        tags_arr.sort();
+                        creations_arr.push(tags_arr);
+                        tags_arr=[];
+                    }
+                }
+            }
+        }
     }
 
     function reduceFunc(tag, counts) {
