c7168de2a9b8da1ca1e6656cabce6b1cc72faeb5
Muhammad <muhammad@matterandform.net>
2015-08-05 17:03:29 -0400

    sort of working with joined array of pairs

--- tag-map-reduce/index.js
+++ tag-map-reduce/index.js
@@ -6,9 +6,31 @@ MongoClient.connect('mongodb://127.0.0.1:27017/matter-and-form-api', function(er
     var creationsCollection = db.collection('creations');
 
     function mapFunc() {
+
+        var lev_dist = function (str1, str2) {
+        var m = str1.length,
+            n = str2.length,
+            d = [],
+            i, j;
+     
+        if (!m) return n;
+        if (!n) return m;
+     
+        for (i = 0; i <= m; i++) d[i] = [i];
+        for (j = 0; j <= n; j++) d[0][j] = j;
+     
+        for (j = 1; j <= n; j++) {
+            for (i = 1; i <= m; i++) {
+                if (str1[i-1] === str2[j-1]) d[i][j] = d[i - 1][j - 1];
+                else d[i][j] = Math.min(d[i-1][j], d[i][j-1], d[i-1][j-1]) + 1;
+            }
+        }
+        return d[m][n];
+    }
+
         var creations_arr = [];
-    	for (var tag in this) {
-            tags = this.tags;
+    	for (var i in this) {
+            var tags = this.tags;
             if (tags.join("") === "1234567891011121314151617181920212223242526") {
                 continue;
             }
@@ -19,7 +41,6 @@ MongoClient.connect('mongodb://127.0.0.1:27017/matter-and-form-api', function(er
                 for (var k = j+1; k < tags.length; k++) {
                     if (tags[j]!==tags[k]) {
                         // No duplicates like ["space", "space"] - we don't want a tag to be identified as being similar to itself 
-                        emit(tag, [tags[k], tags[k]]);
                         tags_arr.push(tags[j]);
                         tags_arr.push(tags[k]);
                         tags_arr.sort();
@@ -28,6 +49,41 @@ MongoClient.connect('mongodb://127.0.0.1:27017/matter-and-form-api', function(er
                     }
                 }
             }
+        }
+        var tag_doubles_count = creations_arr.length;
+        // number of tag doubles
+
+        const pop_group_tag_lim = Math.floor(Math.log(tag_doubles_count) / Math.log(10));
+        // the number of occurrences of a group of tags necessary to make them "popular"
+        // Note: The math makes this program scalable so that it adds 1 to popular_group_tag_limit
+        //     every time the number of tag doubles increases by an order of magnitude in base 10
+
+        const leven_lim = 3;
+        // The maximum Levenshtein distance between two strings in order for them to be considered
+        // similar; 
+
+        var group_tag_count = 0;
+        // number of occurrences of a group of tags in arr
+
+        var popular_tag_groups = [];
+
+        for (var i=0; i<creations_arr.length; i++) {
+            var cur_tag_group_str = creations_arr[i].join("");
+            // parse tags array (double) as string for easy comparisons; this works as tags array is sorted
+            for (var j=i+1; j<creations_arr.length; j++) {
+                var comp_tag_group_str = creations_arr[j].join("");
+                if (lev_dist(comp_tag_group_str, cur_tag_group_str) <= leven_lim) {
+                    group_tag_count++;
+                }
+                if (group_tag_count === pop_group_tag_lim) {
+                    group_tag_count=0;
+                    if (popular_tag_groups.join("").indexOf(creations_arr[j]) === -1) {
+                        emit (creations_arr[j].join(""), 1);
+                        // popular_tag_groups.push(creations_arr[j]);
+                    }
+                }
+            }
+            group_tag_count=0;
         }
     }
 
